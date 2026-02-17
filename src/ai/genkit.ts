c8e3a1b68b1db5a5a135220c2c58d709ab8007e3
idk_name123 <idkname486@gmail.com>
2026-02-17 16:42:35 +0000

    yes my api key is being openely exploit so lets get i inot .env file an

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -5,6 +5,6 @@ import { config } from 'dotenv';
 config();
 
 export const ai = genkit({
-  plugins: [googleAI({apiKey:"AIzaSyC1rbIXbLVTFGQR--aIKpdbRhYyKDs0DFA" })],
+  plugins: [googleAI({apiKey: process.env.GEMINI_API_KEY })],
   model: 'googleai/gemini-2.5-flash',
 });
